0017933e25e355780e469c2d6b83aa360c220dd4
Siyao Xie <siyao1030@gmail.com>
2014-04-10 19:40:59 -0700

    first try

---
+++ app.py
@@ -0,0 +1,98 @@
+from flask import Flask, render_template, request, redirect, url_for
+import pymongo
+import yaml
+
+app = Flask(__name__)
+
+mongodb_uri = 'mongodb://5chackathon:hackweek@ds041367.mongolab.com:41367/hackweek'
+db_name = 'hackweek'
+connection = pymongo.Connection(mongodb_uri)
+
+db = connection[db_name] # or connection.db_name
+
+MAJORS = yaml.load(file('majors.yaml', 'r'))
+
+@app.route('/majors')
+def majors():
+    return render_template('majors.html', majors=MAJORS)
+
+@app.route('/major/<major>/<int:number>', methods=['GET', 'POST'])
+def ratings(major, number):
+    if request.method == 'POST':
+        # Store the review in the database
+        review_text = request.form.get('review',None)
+        name = request.form.get('name',None)
+        review_rating = request.form.get('rating',None)
+
+        if review_text != None:
+            if not name:
+                name = 'Anonymous'
+            review = {
+                'name' : name,
+                'text' : review_text,
+                'major' : major,
+                'course_number' : number,
+                'rating': review_rating
+            }
+
+            db.reviews.insert(review)
+            # Change the request type to GET
+        return redirect(url_for('ratings', major = major, number = number), code=303)
+            # redirect to the url for the ratings function and pass the arguments
+            # and change post to get
+            # 200 ok, 404 page not found, 303 see other page?
+
+    reviews = list(db.reviews.find({'major':major, 'course_number': number}))
+    course = db.course_info.find_one({'major':major, 'course_number':number})
+    #findOne ends after one item has been found
+    
+    #find the reviews
+    avg_rating = None
+    #Once we have the reviews fimd the average rating for the course 
+    if len(reviews) != 0:
+        ratings_sum = sum([int(r.get('rating', 0)) for r in reviews])
+        avg_rating = float(ratings_sum) / len(reviews)
+        avg_rating = round(avg_rating, 2)
+    return render_template("ratings.html", course=course, avg_rating=avg_rating,
+            reviews=reviews)
+
+
+@app.route('/courses')
+def classes_for_major():
+    major_code = request.args.get('major', 'any')
+    school = request.args.get('school', 'any') # any-> get everything
+
+    major = None
+
+    filter_doc = {}
+    if major_code != 'any':
+        filter_doc['major'] = major_code
+        major = MAJORS[major_code]
+    else:
+        major_code = None
+
+    if school != 'any':
+        filter_doc['school'] = school
+    else:
+        school = None
+
+    def by_name(course):
+        return course['name']
+
+    courses = sorted(list(db.course_info.find(filter_doc)), key=by_name) #call to db
+
+
+    #get a listed of the courses offered by name and 
+
+    return render_template("courses.html", courses=courses,
+                           major_code=major_code, major=major)
+
+
+@app.route('/')
+def index():
+    courses = list(db.course_info.find())
+    return render_template('index.html', courses=courses, majors=MAJORS)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
